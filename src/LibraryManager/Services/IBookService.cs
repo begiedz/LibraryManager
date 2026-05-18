9369ba147986d289a4fcbb5920ae2a651eae52ac
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-05-18 12:07:05 +0200

    feat: add BookService Interface

---
+++ src/LibraryManager/Services/IBookService.cs
@@ -0,0 +1,12 @@
+using LibraryManager.Models;
+
+namespace LibraryManager.Services;
+
+public interface IBookService
+{
+    public void AddBook(Book book);
+    public void RemoveBook(Book Book);
+    public List<Book> SearchBooks(string text);
+    public List<Book> SortBooks(string criteria);
+    public void DisplayBooks();
+}
